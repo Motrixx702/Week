c60c93e388707960dbfc17365bdeaf2e7c2fe8df
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-04-02 18:09:24 +0300

    Add files via upload

---
+++ week.sh
@@ -0,0 +1,28 @@
+echo -e "\e[31mWelcome to the tool week"
+echo -e "\e[31mFollow me on my YouTube channel orphan hacker ❤️"
+echo -e "\e[31mFollow me on my Instagram account ahu_orphan"
+echo -e "\e[32mEnter the password to open the tool:"
+read pass
+if [ $pass == orphan ]
+then
+echo -e "\e[32mThe password is correct✅"
+echo -e "\e[33mRegister your own email:"
+#######
+read v
+echo -e "\e[32mYour email was found and matched Successfully..."
+echo -e "\e[33mEnter the port you want to open:"
+read f
+echo -e "\e[35mThe port is now open, press Enter to complete the process..:"
+read h
+#######
+echo -e "\e[32mThe port has been opened successfully..."
+#######
+echo -e "\e[32mOnline"
+echo -e "\e[32mLatency:33ms"
+echo -e "\e[32mWeb Interfac:http://127.230.0.1:4040"
+echo -e "\e[32mForwarding:tcp://0.tcp.eu.ngrok.io:18262 -> localhost:5252"
+echo -e "\e[32mConnections:     ttl:0     opn:0     rt1:0.00     rt5:0.00     p50:0.00     p90:0.00"
+
+fi 
+	exit
+
